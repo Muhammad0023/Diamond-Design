c2e451f618e6f85c52dc977dacd548e8a87726d9
muhammad <muhe098@gmail.com>
2025-12-23 14:44:45 +0300

    add components and page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ function App() {
       <button className="bg-[#D4AF37] text-white px-6 py-3 rounded-full font-bold shadow-lg hover:scale-105 transition-transform">
         Habesha Kemis Store - Tailwind v4 is Live!
       </button>
+      
+      <button className="bg-brand text-white px-6 py-3 rounded-full font-bold shadow-lg hover:scale-105 hover:shadow-xl transition-all duration-300">
+  Add to Cart
+</button>
     </div>
   )
 }
